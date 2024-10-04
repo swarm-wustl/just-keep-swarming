6e2b2e36d6d88beb64758abfbcbf28e5ef261c6f
jaxon <jaximus808@gmail.com>
2024-10-04 18:51:16 -0500

    feat(map): Implemented code to generate OG grid and process map meta data

--- ros/src/camera_feed/camera_feed/camera_feed.py
+++ ros/src/camera_feed/camera_feed/camera_feed.py
@@ -4,11 +4,68 @@ import rclpy
 import rclpy.exceptions
 import rclpy.executors
 import rclpy.logging
-
 from rclpy.node import Node
 
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
+
+from shared_types.srv import CamMeta
+
+from std_msgs.msg import Float32MultiArray
+
+
+# Assuming we are using meters
+class CamData:
+    def __init__(self, cam_node):
+
+        cam_node.declare_parameter("delay", 0.001)
+
+        self.delay = cam_node.get_parameter("delay").value
+
+        cam_node.declare_parameter("focal_length", -1.0)
+
+        self.focal_length = cam_node.get_parameter("focal_length").value
+
+        cam_node.declare_parameter("cam_height", -1.0)
+
+        self.cam_height = cam_node.get_parameter("cam_height").value
+
+        self.shape = [0.0, 0.0]
+
+        # in future when camera can move
+        self.delt_move = (0.0, 0.0)
+
+        self.shape_publisher = cam_node.create_publisher(
+            Float32MultiArray, "/img_shape", 10
+        )
+
+        self.data_service = cam_node.create_service(
+            CamMeta, "cam_meta_data", self.get_meta_data
+        )
+
+    def get_meta_data(self, request, response):
+        conv = 1.0
+        match request.units:
+            case "cm":
+                conv = 0.01
+
+        response.focal_length = self.focal_length * conv
+        response.cam_height = self.cam_height * conv
+        response.width = self.shape[0]
+        response.height = self.shape[1]
+        return response
+
+    def update_size(self, width, height):
+        if (self.shape[0], self.shape[1]) == (width, height):
+            return
+
+        self.shape[0] = width
+        self.shape[1] = height
+
+        update_data = Float32MultiArray()
+        update_data.data = self.shape
+        print(self.shape)
+        self.shape_publisher.publish(update_data)
 
 
 class CameraFeed(Node):
@@ -16,19 +73,31 @@ class CameraFeed(Node):
         super().__init__("camera_feed")
         self.bridge = CvBridge()
 
-        self.declare_parameter("delay", 0.001)
-        delay = self.get_parameter("delay").value
+        self.cam_data = CamData(self)
+
+        if self.cam_data.focal_length <= 0 or self.cam_data.cam_height <= 0:
+            raise Exception("valid focal_length and cam_height is a required argument")
+
         self.cap = cv2.VideoCapture(0)
-        self.publisher = self.create_publisher(Image, "/video", 10)
-        self.timer = self.create_timer(delay, self.run)
+
+        self.video_publisher = self.create_publisher(Image, "/video", 10)
+
+        self.timer = self.create_timer(self.cam_data.delay, self.run)
 
     # gets the camera feed and publishes it to /video topic
+
     def run(self):
         try:
             ret, frame = self.cap.read()
             if not ret:
                 return
-            self.publisher.publish(self.bridge.cv2_to_imgmsg(frame, "bgr8"))
+
+            # just in case something updates
+            self.cam_data.update_size(
+                width=float(frame.shape[1]), height=float(frame.shape[0])
+            )
+
+            self.video_publisher.publish(self.bridge.cv2_to_imgmsg(frame, "bgr8"))
 
         except CvBridgeError as error:
             rclpy.shutdown()
---
+++ ros/src/map_maker/map_maker/map_maker.py
@@ -0,0 +1,85 @@
+import rclpy
+import rclpy.exceptions
+import rclpy.executors
+import rclpy.logging
+
+from rclpy.node import Node
+
+from shared_types.srv import CamMeta
+
+from std_msgs.msg import Float32MultiArray
+
+
+class MapMaker(Node):
+    def __init__(self):
+        super().__init__("map_maker")
+        self.map_meta_client = self.create_client(CamMeta, "cam_meta_data")
+        while not self.map_meta_client.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info("waiting for cam node")
+        future = self.get_request()
+        rclpy.spin_until_future_complete(self, future)
+        response = future.result()
+
+        self.robot_pos_sub = self.create_subscription(
+            msg_type=Float32MultiArray,
+            topic="/multi_array_pos",
+            callback=self.bot_update_callback,
+            qos_profile=10,
+        )
+
+        self.map_size_update = self.create_subscription(
+            msg_type=Float32MultiArray,
+            topic="/multi_array_pos",
+            callback=self.set_map,
+            qos_profile=10,
+        )
+
+        self.robots = {}
+
+        self.cam_data = {
+            "focal_length": response.focal_length,
+            "cam_height": response.cam_height,
+        }
+        self.map = [0]
+        self.cam_data = [self.cam_data["focal_length"], self.cam_data["cam_height"]]
+        self.set_map(
+            [
+                response.width,
+                response.height,
+            ]
+        )
+        # from here a map will be generated
+        # print(response)
+
+    def bot_update_callback(self, data):
+
+        data_n = len(data)
+        for robo_it in range(0, data_n, 9):
+            robo_id = data[robo_it]
+
+            self.robots[robo_id] = [
+                (data[robo_it + 1], data[robo_it + 2]),
+                (data[robo_it + 3], data[robo_it + 4]),
+                (data[robo_it + 5], data[robo_it + 6]),
+                (data[robo_it + 7], data[robo_it + 8]),
+            ]
+        ## draw in map after
+
+    def set_map(self, data):
+        print(data)
+        self.map = [0] * (int(data[0] * data[1]))
+        print(self.map)
+
+    def get_request(self):
+        req = CamMeta.Request()
+        req.units = "m"
+        return self.map_meta_client.call_async(req)
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    map_maker = MapMaker()
+
+    print("generating map")
+    rclpy.spin(map_maker)
