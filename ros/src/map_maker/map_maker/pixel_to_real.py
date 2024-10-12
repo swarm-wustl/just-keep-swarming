6e2f3e5aacc6c4062248e67d9a3f55ce432a2a74
e.joo <ejoo@Swarm.myguest.virtualbox.org>
2024-10-12 12:53:02 -0500

    fixed typo

--- ros/src/map_maker/map_maker/pixel_to_real.py
+++ ros/src/map_maker/map_maker/pixel_to_real.py
@@ -7,7 +7,7 @@ def pixel_to_world(pixel_coords, focal_length, camera_height, sensor_size, image
     image_width, image_height = image_resolution
    
     x_norm = x * (sensor_width / image_width)
-    y_norm = y (sensor_height / image_height)
+    y_norm = y * (sensor_height / image_height)
    
     X = (x_norm * camera_height) / focal_length
     Y = (y_norm * camera_height) / focal_length
