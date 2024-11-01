0114bb819d98edcf6092148af21e26dd702521fd
prestonmeek <meek@wustl.edu>
2024-11-01 18:25:28 -0500

    feat(embedded): Got custom ROS messages able to be included. Still need to test their functionality

--- embedded/esp/main/main.c
+++ embedded/esp/main/main.c
@@ -16,6 +16,8 @@
 
 #include "receiver.h"
 
+#include <my_custom_message/my_custom_message/msg/my_custom_message.h>
+
 #define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){printf("Failed status on line %d: %d. Aborting.\n",__LINE__,(int)temp_rc);vTaskDelete(NULL);}}
 
 void subscription_callback(const void *msgin) {
