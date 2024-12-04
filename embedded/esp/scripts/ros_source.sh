fc11a3217062c9a68f27d5b6ace3a6f23bf875ac
prestonmeek <meek@wustl.edu>
2024-12-04 16:01:48 -0600

    fix(embedded): Updated sourcing script to work as intended, added uros dameon script

--- embedded/esp/scripts/ros_source.sh
+++ embedded/esp/scripts/ros_source.sh
@@ -1,7 +1,7 @@
 # Make sure this is run in its parent directory (i.e., esp)
-# ex: sh scripts/[name].sh
+# eg: source scripts/[name].sh
+# MUST BE RUN AS A SOURCE COMMAND
 . ~/ros2_humble/install/setup.sh
 cd ./components/micro_ros_espidf_component/extra_packages
 colcon build
-cd -
-tset
+. ./install/setup.sh
