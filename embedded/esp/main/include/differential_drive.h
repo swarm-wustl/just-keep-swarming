7df56486e38b1aa7658d793700ed9744f246d893
prestonmeek <meek@wustl.edu>
2025-01-20 23:49:53 -0600

    moved some defines from header to source

--- embedded/esp/main/include/differential_drive.h
+++ embedded/esp/main/include/differential_drive.h
@@ -4,24 +4,6 @@
 #include "motor_driver.h"
 #include "command_parser.h"
 
-#define LEDC_TIMER LEDC_TIMER_0
-#define LEDC_MODE LEDC_LOW_SPEED_MODE
-#define LEDC_DUTY_RES LEDC_TIMER_10_BIT
-#define LEDC_FREQUENCY 1000 // Hz
-
-#define LEFT_MOTOR_CHANNEL LEDC_CHANNEL_0
-#define RIGHT_MOTOR_CHANNEL LEDC_CHANNEL_1
-
-#define ENA GPIO_NUM_13
-#define IN1 GPIO_NUM_12
-#define IN2 GPIO_NUM_14
-#define ENB GPIO_NUM_14   // TODO
-#define IN3 GPIO_NUM_27
-#define IN4 GPIO_NUM_26
-#define GPIO_BITMASK (1ULL << ENA) | (1ULL << IN1) | (1ULL << IN2) | (1ULL << ENB) | (1ULL << IN3) | (1ULL << IN4)
-
-#define PWM_STOP 0.0
-
 typedef struct {
     motor_direction_t left_dir;
     motor_direction_t right_dir;
--- embedded/esp/main/src/differential_drive.c
+++ embedded/esp/main/src/differential_drive.c
@@ -21,6 +21,22 @@
 
 #define WHEELBASE 0.5   // dist between wheels (m)
 
+#define LEDC_TIMER LEDC_TIMER_0
+#define LEDC_MODE LEDC_LOW_SPEED_MODE
+#define LEDC_DUTY_RES LEDC_TIMER_10_BIT
+#define LEDC_FREQUENCY 1000 // Hz
+
+#define LEFT_MOTOR_CHANNEL LEDC_CHANNEL_0
+#define RIGHT_MOTOR_CHANNEL LEDC_CHANNEL_1
+
+#define ENA GPIO_NUM_13
+#define IN1 GPIO_NUM_12
+#define IN2 GPIO_NUM_14
+#define ENB GPIO_NUM_14   // TODO
+#define IN3 GPIO_NUM_27
+#define IN4 GPIO_NUM_26
+#define GPIO_BITMASK (1ULL << ENA) | (1ULL << IN1) | (1ULL << IN2) | (1ULL << ENB) | (1ULL << IN3) | (1ULL << IN4)
+
 command_parser_ret_t twist_to_differential_drive(const geometry_msgs__msg__Twist *msgin, differential_drive_motor_command_t *msgout) {
     if (msgin == NULL || msgout == NULL) {
         return COMMAND_PARSER_ERROR_GENERIC;
