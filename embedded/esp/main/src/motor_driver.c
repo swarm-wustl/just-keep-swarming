488bb2b20c9b2e2b3e33c89c82154ee7d7b4954b
prestonmeek <meek@wustl.edu>
2025-01-29 17:13:18 -0600

    finished TODOs in motor_driver.c

--- embedded/esp/main/src/motor_driver.c
+++ embedded/esp/main/src/motor_driver.c
@@ -27,29 +27,28 @@ void push_to_motor_driver_queue(void *msgin) {
 void motor_driver_task(motor_driver_t *driver) {
     if (driver->init() != MOTOR_DRIVER_SUCCESS) {
         printf("Motor driver init failed. Exiting task...\n");
-	vTaskDelete(NULL);
+	    vTaskDelete(NULL);
     }
 
     xQueue = xQueueCreate(MOTOR_DRIVER_QUEUE_SIZE, sizeof(void *));
 
-    while (1) {
-        if (xQueue == NULL) {
-            // TODO: error handler
-            continue;
-        }
+    if (xQueue == NULL) {
+        printf("xQueue init failed. Exiting task...\n");
+        vTaskDelete(NULL); 
+    }
 
+
+    while (1) { 
         if (xQueueReceive(xQueue, &msgin, delay) != pdPASS) {
-            // TODO: error handler
             continue;
         }
 
-        printf("received data!\n");
+        printf("Received data!\n");
 
         differential_drive_motor_command_t * temp = msgin;
-        printf("received msgaa! %f\n", temp->left_pwm_ratio);
 
         if (driver->handler(msgin) != MOTOR_DRIVER_SUCCESS) {
-            // TODO: error handler
+            printf("Motor driver handler failed to parse message. Continuing task...\n");
             continue;
         }
     }
@@ -57,5 +56,6 @@ void motor_driver_task(motor_driver_t *driver) {
     // clean up memory (if task ever reaches this point)
     free(msgin);
     free(driver);
+
     vTaskDelete(NULL);
 }
