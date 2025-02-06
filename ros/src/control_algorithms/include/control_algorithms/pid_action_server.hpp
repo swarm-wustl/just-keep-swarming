754845ace0d45a4a39e7c2a947142195b57800de
prestonmeek <meek@wustl.edu>
2025-02-05 18:06:50 -0600

    got PID action server to build

--- ros/src/control_algorithms/include/control_algorithms/pid_action_server.hpp
+++ ros/src/control_algorithms/include/control_algorithms/pid_action_server.hpp
@@ -4,10 +4,7 @@
 #include <set>
 #include <vector>
 
-#include "control_algorithms/action/multi_robot_path_plan.hpp"
-#include "drone_msg/msg/robot_position.hpp"
-#include "geometry_msgs/msg/pose_array.hpp"
-#include "nav_msgs/msg/occupancy_grid.hpp"
+#include "control_algorithms/action/pid.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp_action/rclcpp_action.hpp"
 
@@ -37,8 +34,22 @@ class PIDActionServer : public rclcpp::Node {
         using PID = control_algorithms::action::PID;
         using GoalHandlePID = rclcpp_action::ServerGoalHandle<PID>;
 
+        PIDActionServer(const rclcpp::NodeOptions &options);
+
+        rclcpp_action::GoalResponse handle_goal(
+            const rclcpp_action::GoalUUID &uuid,
+            std::shared_ptr<const PID::Goal> goal);
+
+        rclcpp_action::CancelResponse handle_cancel(
+            const std::shared_ptr<GoalHandlePID> goal_handle);
+
+        void handle_accepted(
+            const std::shared_ptr<GoalHandlePID> goal_handle);
+
+        void execute(const std::shared_ptr<GoalHandlePID> goal_handle);
+
     private:
-        rclcpp_action::Server<Fibonacci>::SharedPtr action_server_;
-}
+        rclcpp_action::Server<PID>::SharedPtr action_server_;
+};
 
 }
--- ros/src/control_algorithms/src/pid_action_server.cpp
+++ ros/src/control_algorithms/src/pid_action_server.cpp
@@ -1,4 +1,4 @@
-#include "pid_action_server.hpp"
+#include "control_algorithms/pid_action_server.hpp"
 
 namespace control_algorithms {
 
@@ -7,14 +7,36 @@ PIDActionServer::PIDActionServer(const rclcpp::NodeOptions &options)
     
     using namespace std::placeholders;
 
-    this->action_server_ = rclcpp_action::create_server<Assemble>(
+    this->action_server_ = rclcpp_action::create_server<PID>(
       this, "pid",
-      std::bind(&AssemblerActionServer::handle_goal, this, _1, _2),
-      std::bind(&AssemblerActionServer::handle_cancel, this, _1),
-      std::bind(&AssemblerActionServer::handle_accepted, this, _1)
+      std::bind(&PIDActionServer::handle_goal, this, _1, _2),
+      std::bind(&PIDActionServer::handle_cancel, this, _1),
+      std::bind(&PIDActionServer::handle_accepted, this, _1)
     );
 
     RCLCPP_INFO(this->get_logger(), "PID action server initialized");
+}
+
+rclcpp_action::GoalResponse PIDActionServer::handle_goal(
+        const rclcpp_action::GoalUUID &uuid,
+        std::shared_ptr<const PID::Goal> goal) {
+    RCLCPP_INFO(this->get_logger(), "Received goal request");
+    (void)uuid;
+    (void)goal;
+    return rclcpp_action::GoalResponse::ACCEPT_AND_EXECUTE;
+}
+
+rclcpp_action::CancelResponse PIDActionServer::handle_cancel(
+        const std::shared_ptr<GoalHandlePID> goal_handle) {
+    RCLCPP_INFO(this->get_logger(), "Received request to cancel goal");
+    (void)goal_handle;
+    return rclcpp_action::CancelResponse::ACCEPT;
+}
+
+void PIDActionServer::handle_accepted(
+        const std::shared_ptr<GoalHandlePID> goal_handle) {
+    using namespace std::placeholders;
+    std::thread{std::bind(&PIDActionServer::execute, this, _1), goal_handle}.detach();
 }
 
 }
