c716da1785f8f12480226dd7af989f69b788fc2f
prestonmeek <meek@wustl.edu>
2025-04-05 13:17:52 -0500

    added comments for future testing

--- embedded/esp/main/src/main.c
+++ embedded/esp/main/src/main.c
@@ -48,3 +48,4 @@ void app_main(void)
 }
 
 // ros2 topic pub /diffdrive_twist geometry_msgs/msg/Twist '{linear: {x: 0.5, y: 0.0, z: 0.0}, angular: {x: 0.0, y: 0.0, z: 1.0}}'
+// source /opt/ros/${ROS_DISTRO}/setup.bash
--- embedded/esp/scripts/ros_test.sh
+++ embedded/esp/scripts/ros_test.sh
@@ -1,3 +1,3 @@
 echo "ARG: $1"
 . ~/ros2_humble/install/setup.sh
-ros2 topic pub /diffdrive_twist_$1 geometry_msgs/msg/Twist '{linear: {x: 0.0, y: 0.0, z: 0.0}, angular: {x: 1.0, y: 0.0, z: 0.0}}'
+ros2 topic pub /diffdrive_twist_$1 geometry_msgs/msg/Twist '{linear: {x: 1.0, y: 0.0, z: 0.0}, angular: {x: 0.0, y: 0.0, z: 0.0}}'
