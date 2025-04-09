9f0a07fb886d2e0bd77ac995d016b77da3e3a038
jaxon <jaximus808@gmail.com>
2025-04-09 18:14:33 -0500

    feat(pid): Modified PID to work with one robot

--- ros/src/overhead_cv/overhead_cv/utils/robot_estimator.py
+++ ros/src/overhead_cv/overhead_cv/utils/robot_estimator.py
@@ -1,7 +1,6 @@
 import time
 
 import numpy as np
-
 from overhead_cv.utils.filtering_types import Command, Measurement, State
 from overhead_cv.utils.kalman_filter import kalman_filter
 
@@ -10,7 +9,7 @@ def low_pass_filter(new_angle, prev_filtered, alpha=0.15):
     return alpha * new_angle + (1 - alpha) * prev_filtered
 
 
-# pylint: disable=too-few-public-methods
+# pylint: disable=too-few-public-methods,too-many-instance-attributes
 class RobotStateEstimator:
     def __init__(self, x=State(), q=0.09, r=0.005):
         self.x = x
@@ -21,25 +20,27 @@ class RobotStateEstimator:
         self.last_estimate_received_at = 0
         self.dist_threshold = 0.20  # meters
         self.ang_threshold = 40 * np.pi / 180  # rad
+        # self.reset_counter = 0
 
     def update_estimate(self, u: Command, z: Measurement, dt=1.0, save=True):
         if not self.num_estimates_received:  # or self.reset_counter > 20:
             self.x = State(z.x, z.y, 0, z.theta)
-
-        # if z.theta == 0:
-        #     return self.x, self.P
-        # if (
-        #     math.hypot(self.x.x - z.x, self.x.y - z.y) > self.DIST_THRESHOLD
-        #     or abs(self.x.orientation - z.theta) > self.ANG_THRESHOLD
-        # ) and self.num_estimates_received > 50:
-        #     print("unreliable")
-        #     print(math.hypot(self.x.x - z.x, self.x.y - z.y), self.DIST_THRESHOLD)
-        #     print(abs(self.x.orientation - z.theta), self.ANG_THRESHOLD)
-        #     self.reset_counter += 1
-        #     # unreliable measurement
-        #     return self.x, self.P
+            # self.reset_counter = 0
         # else:
-        #     self.reset_counter = 0
+        #     if z.theta == 0:
+        #         return self.x, self.P
+        #     if (
+        #         math.hypot(self.x.x - z.x, self.x.y - z.y) > self.dist_threshold
+        #         or abs(self.x.orientation - z.theta) > self.ang_threshold
+        #     ) and self.num_estimates_received > 50:
+        #         print("unreliable")
+        #         print(math.hypot(self.x.x - z.x, self.x.y - z.y), self.dist_threshold)
+        #         print(abs(self.x.orientation - z.theta), self.ang_threshold)
+        #         self.reset_counter += 1
+        #         # unreliable measurement
+        #         return self.x, self.P
+        #     else:
+        #         self.reset_counter = 0
 
         x, P = kalman_filter(
             self.x.to_np(), u.to_np(), z.to_np(), self.P, dt, self.q, self.r
