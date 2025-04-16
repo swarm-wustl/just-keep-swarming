3b50d9a4ec1f934700dfc5c74369551e186523a9
Sebastian Theiler <25444757+sebtheiler@users.noreply.github.com>
2025-04-16 18:39:06 -0500

    feat(control_algorithms): Tried to multithread PIDAS, might be cooked

--- ros/src/control_algorithms/include/control_algorithms/pid_action_server.hpp
+++ ros/src/control_algorithms/include/control_algorithms/pid_action_server.hpp
@@ -1,43 +1,16 @@
 // Copyright 2024 Prestin Meek
 #pragma once
-// // Copyright 2024 Prestin Meek
-// #pragma once
 
 #include <memory>
 #include <set>
-#include <unordered_map>
 #include <vector>
 
 #include "control_algorithms/action/pid.hpp"
-#include "geometry_msgs/msg/twist_stamped.hpp"
 #include "geometry_msgs/msg/pose_stamped.hpp"
+#include "geometry_msgs/msg/twist_stamped.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp_action/rclcpp_action.hpp"
 #include "shared_types/msg/pid_position.hpp"
-/*
- * camera node initializes all ESP32s
- * camera node tells PID controller: hey, this robot exists, here's my ID,
- make
- * a topic for me PID controller responds: ok, here's a topic for the robot
- * camera node sends ID back to ESP32
- * ESP32 subscribes to PID topic
- */
-
-/*
-* subscribe to camera topic
-* callback on new data
-* for each robot, run pid
-* pid fucking somehow has the target position
-* pid sends data to each robot
-
-* TODO: make flowchart
-*/
-
-/*
-
-    One thing to note, check if execute runs multipel times
-
-*/
 
 namespace control_algorithms {
 
@@ -63,18 +36,8 @@ class PIDActionServer : public rclcpp::Node {
 
  private:
   rclcpp_action::Server<PID>::SharedPtr action_server_;
-
-  rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr robot_pub_;
-
-  std::unordered_map<uint32_t, geometry_msgs::msg::Pose> robot_map_;
-
-  std::mutex position_mutex_;
-
   bool is_sim;
   int debug_lvl;
-
-  // could just make this an anon function
-  void position_callback_(const shared_types::msg::PidPosition pos_msg_);
 };
 
 }  // namespace control_algorithms
