90dca6dba08f1486d11157ee123480ec10e2b617
Sebastian Theiler <25444757+sebtheiler@users.noreply.github.com>
2025-08-11 22:29:11 -0400

    feat(nix): Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Swarm";
+  inputs = {
+    nix-ros-overlay.url = "github:lopsided98/nix-ros-overlay/master";
+    nixpkgs.follows = "nix-ros-overlay/nixpkgs";
+  };
+  outputs = { self, nix-ros-overlay, nixpkgs }:
+    nix-ros-overlay.inputs.flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ nix-ros-overlay.overlays.default ];
+        };
+      in {
+        devShells.default = pkgs.mkShell {
+          name = "Swarm";
+          packages = with pkgs; [
+            colcon
+            # ... other non-ROS packages
+            (with rosPackages.humble; buildEnv {
+              paths = [
+                ros-core
+                # ... other ROS packages
+              ];
+            })
+          ];
+        };
+      });
+  nixConfig = {
+    extra-substituters = [ "https://ros.cachix.org" ];
+    extra-trusted-public-keys = [ "ros.cachix.org-1:dSyZxI8geDCJrwgvCOHDoAfOm5sV1wCPjBkKL+38Rvo=" ];
+  };
+}
